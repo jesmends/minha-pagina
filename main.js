d3bec15f1c1b1eb1dbf4f7705c23ec7cf2f2fc36
jesmends <je.mends@gmail.com>
2024-07-01 19:29:26 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,134 @@
+// Variáveis
+let nome = 'Jéssica Mendes'
+let cargoAtual = 'Analista de Testes | QA';
+let telefone = 'Telefone: (67) 9 9875-7417';
+let email = 'E-mail: je.mends@hotmail.com';
+
+// Variáveis com textos grandes
+    // É boa prática colocar textos grandes assim? Não sei.
+
+let aboutMe = `
+    <p>Oi, sou a Jéssica, tenho 32 anos moro em Campo Grande no MS.</p>
+    <p>Sou casada, mãe da Maria e uma millenial clássica. Gosto de ler, dias cinzas, cozinhar e estudar (eu juro!).</p>
+    <br>
+    <p>Minha carreira começou bacharel em Direito e hoje estou aqui, <strong>Analista de Qualidade|QA e Desenvolvedora front-end(em formação) focada na qualidade e experiência do usuário.</strong> O mundo gira não é mesmo?</p>
+    <p>Mas vamos contar melhor essa história...<p/>
+    <br>
+    <p>Minha jornada com a TI e com a qualidade do software começou em 2014 como Assistente de Suporte Técnico. Nessa função, meu trabalho era garantir que todos os funcionários da empresa tivessem pleno acesso aos sistemas, isso incluia a liberação e a manutenção desses acessos.</p>
+    <p>Cada vez que um erro era apresentado eu testava até encontrar a lógica, corrigia (caso me coubesse) ou especificava e mandava para correção (caso estivesse fora do meu escopo), e então testava novamente quando a demanda retornava.</p>
+    <p>Em 2016, fui promovida a Analista nesse setor e minhas atribuições passaram a incluir projetos, o que significava estar envolvida nas implantações de novos sistemas.</p>
+    <p>Foi só em 2021 que me aproximei do desenvolvimento de forma mais direta. Nos últimos anos venho me dedicando à parte técnica e me especializando em automações e testes de API.</p>
+    <br>
+    <p><strong>Atualmente faço parte da equipe da Funcional Health Tech</strong>, uma empresa dedicada a desenvolver soluções tecnológicas para fazer o ecossistema da saúde funcionar de maneira mais inteligente e sustentável, atuo no projeto Trade que é focado na relação entre Indústria, Distribuidor e Lojista.</p>
+    <p>Além disso <strong>estou cursando o 4º período de Análise e Desenvolvimento de Sistemas</strong>, ao longo do curso descobri minha paixão pelo desenvolvimento front-end então decidi unir minha experiência com qualidade de software e cohecimento em experiência do usuário e passar a também desenvolver.</p>
+    <p>Paralelamente a isso venho fazendo cursos e trabalhando em projetos pessoais para ampliar minha experiência e conhecimento nas linguagens HTML, CSS e JavaScript.</p>
+    `;
+let habilidades = `
+    <p>Scrum | Kanban | Gherkin | BDD | Confluence | Robot Framework | Visual Studio Code | Postman | Jira |
+                Docker | GIt | GitLab | Linux | API GraphQL | API REST | Android Studio | Insomnia | Altair | JSON |
+                Logs | MongoDB | Postgres | MySQL | Snowflake | Shift Left | CI/CD | | AzureDevops | Básico de ITIL |
+                BDD | CSS | HTML | JavaScript | Cloud | </p>
+    `;
+let historico = `
+            <h3>Analista de Qualidade - Funcional Health Tech - De 02/2023 - atual -Remoto.</h3>
+            <p>Como analista de qualidade, trabalho em colaboração com o Product Owner para garantir a melhor
+                experiência do usuário em cada entrega; Contribuo ativamente nos Refinamentos; Especifico os cenários de
+                testes para orientar o desenvolvimento; Meu foco está na realização de testes funcionais em ambiente web
+                e mobile validando correções e implementações e garantindo a estabilidade do sistema por meio de testes
+                de regressão. Além disso, sou responsável pelo desenvolvimento e manutenção de códigos para automação de
+                teste E2E, bem como pela execução dos testes de API. Tenho experiência com bancos de dados relacionais e
+                não relacionais e com a documentação das funcionalidades do sistema e dos processos internos da empresa.
+            </p>
+            <h3>Analista de Testes - Nota Control - De 11/2021 a 01/2023 - Presencial.</h3>
+            <p>Como Tester, realizava análise de chamados vindos da triagem para especificação de tasks utilizando
+                linguagem Gherkin e User Story; Era responsável pela realização de testes manuais funcionais e de
+                sistema garantindo a devida implementação das alterações, o correto relacionamento com Banco de Dados e
+                o pleno funcionamento do sistema; Além disso, conduzia testes de regressão para identificar e corrigir
+                possíveis falhas, e realizava testes de Nota Fiscal em XML utilizando API. Minha experiência inclui o
+                uso de bancos de dados relacionais, envolvendo a criação de scripts e a interpretação de procedures.
+                Também desempenhei atividades como rastreamento, a documentação e reporte de defeitos, além de
+                contribuir para a documentação dos processos internos e regras de negócio na Wiki da empresa.
+                Demonstrei iniciativa ao iniciar a implementação de testes automatizados utilizando Selenium, Cucumber e
+                Intellij Idea, visando aumentar a eficiência e a qualidade dos processos de teste.</p>
+            <h3>Analista de Sistemas de Negócios - Tahto - De 08/2016 a 08/2021 - Híbrido.</h3>
+            <p>Na minha função como Analista de Sistemas de Negócios fui responsável por tratar, especificar e monitorar
+                demandas diárias relacionadas a correções e melhorias sistêmicas. Meu foco principal estava na gestão de
+                acessos, garantindo que todos os colaboradores da Tahto tivessem acesso adequado, além de corrigir
+                eventuais falhas operacionais; Participei de projetos de implementação de software, desde sua fase
+                inicial de levantamento de requisitos e reuniões de alinhamento até sua fase final com execução de
+                testes. Durante todo o processo, negociei entregas e prazos com demandantes e clientes finais. </p>
+    `;
+let formacao = `
+            <h3>Direito</h3>
+            <p>Uniderp, concluído em 2015. </p>
+            <h3>Análise e Desenvolvimento de Sistemas</h3>
+            <p> Estácio, cursando.</p>
+    `;
+let cursos = `
+        <p>- Administração de Banco de Dados.</p>
+        <p>- Google Cloud Computing Foundations Certificate: Networking | Infrastructure | Data, ML and AI.</p>
+        <p>- Confluence Fundamentals Badge.</p>
+        <p>- HTML e CSS: ambientes de desenvolvimento, estrutura de arquivos e tags | Trabalhando com responsividade e publicação de projetos | Cabeçalho, footer e variáveis CSS | Classes, posicionamento e Flexbox.</p>
+        <p>- Lógica de programação.</p>
+        <p>- JavaScript: Manipulando o DOM | Crie páginas dinâmicas.</p>
+        <p>- Trilha Digital | Coders 24 | Front End.</p>
+        <p>- Git para testers.</p>
+        <p>- Imersão em Inteligência Artificial.</p>
+    `;
+
+// Função para exibir os textos.
+function exibirTextoEmTela(tag,texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+// Dados pessoais. 
+exibirTextoEmTela('h1',nome)
+exibirTextoEmTela('h2',cargoAtual);
+exibirTextoEmTela('#telefone',telefone);
+exibirTextoEmTela('#email',email);
+
+// Informações
+exibirTextoEmTela('#sobre',aboutMe);
+exibirTextoEmTela('#skills',habilidades);
+exibirTextoEmTela('#historico-profissional',historico);
+exibirTextoEmTela('#faculdade',formacao);
+exibirTextoEmTela('#curso-certificado',cursos);
+
+
+// Função de redirecionamento de links.
+function redirecionarPagina(tag,url){
+    let site = document.querySelector(tag);
+
+    site.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.open (url, '_blank');
+    });
+}
+
+// Links
+redirecionarPagina('#linkedin','https://www.linkedin.com/in/jesmends/')
+redirecionarPagina('#github','https://github.com/jesmends')
+
+//Menu hamburguer 
+document.addEventListener('DOMContentLoaded', () => {
+    let icone = document.querySelector('.material-symbols-outlined');
+    let itens = document.querySelector('#itens');
+    let menuLinks = itens.querySelectorAll('a');
+
+    icone.addEventListener('click', () => {
+        if (itens.style.display == 'flex') {
+                itens.style.display = 'none';
+        } else {
+                itens.style.display = 'flex';
+        }
+        });
+
+    Array.from(menuLinks).forEach(function(link) {
+        link.addEventListener('click', () => {
+            itens.style.display = 'none';
+        });
+    });
+    
+});
+
